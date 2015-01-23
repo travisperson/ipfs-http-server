8d8ff625cc66bb94e20ea632e5d26de3a874835e
Travis Person <travis.person@gmail.com>
2015-01-22 20:37:18 -0800

    Removed fixed dir and added error handling

--- ipfs-http-server.go
+++ ipfs-http-server.go
@@ -1,13 +1,14 @@
 package main
 
 import (
+	"fmt"
 	"code.google.com/p/go.net/context"
 	core "github.com/jbenet/go-ipfs/core"
 	coreunix "github.com/jbenet/go-ipfs/core/coreunix"
 	fsrepo "github.com/jbenet/go-ipfs/repo/fsrepo"
-	u "github.com/jbenet/go-ipfs/util"
 	"io"
 	"net/http"
+	"os/user"
 )
 
 type IPFSHandler struct {
@@ -17,7 +18,7 @@ type IPFSHandler struct {
 
 
 func (p *IPFSHandler) Init(repo string) {
-	p.repo = fsrepo.At("/home/travis/.go-ipfs")
+	p.repo = fsrepo.At(repo)
 	err := p.repo.Open()
 	if err != nil {
 		panic(err)
@@ -31,10 +32,21 @@ func (p *IPFSHandler) Init(repo string) {
 
 func (p *IPFSHandler) Get(w http.ResponseWriter, r *http.Request) {
 	blk := r.URL.Path[6:]
-	reader, err := coreunix.Cat(p.node, u.B58KeyDecode(blk))
+
+	if len(blk) < 3 {
+		w.WriteHeader(403)
+		io.WriteString(w, "Hash must be longer than 3 bytes")
+		return
+	}
+
+	fmt.Println(blk)
+	
+	reader, err := coreunix.Cat(p.node, blk)
 
 	if err != nil {
-		panic(err)
+		w.WriteHeader(404)
+		io.WriteString(w, "Failed to retrieve: " + blk)
+		return
 	}
 
 	io.Copy(w, reader)
@@ -42,15 +54,16 @@ func (p *IPFSHandler) Get(w http.ResponseWriter, r *http.Request) {
 func main() {
 
 	ipfs := IPFSHandler{}
+	
+	usr, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
 
-	ipfs.Init("/home/travis/.go-ipfs")
+	ipfs.Init(usr.HomeDir + "/.go-ipfs")
 
-	//r := mux.NewRouter()
-	//r.PathPrefix("/ipfs/{hash}").HandlerFunc(ipfs.Get)
-	//r.PathPrefix("/").Handler(http.FileServer(http.Dir(".")))
 	http.HandleFunc("/ipfs/", ipfs.Get)
 	http.Handle("/", http.FileServer(http.Dir(".")))
-
 
 	http.ListenAndServe(":8080", nil)
 }
